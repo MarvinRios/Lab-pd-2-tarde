a741200a64463170945b638b495461d57caa055d
Marvin Rios <mriost1@upao.edu.pe>
2026-04-26 12:43:40 -0500

    feat(java): agregar lógica en la clase principal para mostrar el resultado

--- src/lab02patrones/Lab02Patrones.java
+++ src/lab02patrones/Lab02Patrones.java
@@ -11,24 +11,28 @@ public class Lab02Patrones {
         PatoReal p1 = new PatoReal();
         p1.mostrar();
         p1.realizarCuaqueo();
+        p1.realizarVuelo();
         p1.nadar();
         System.out.println("\n");
         
         PatoDeGoma p2 = new PatoDeGoma();
         p2.mostrar();
         p2.realizarCuaqueo();
+        p2.realizarVuelo();
         p2.nadar();
         System.out.println("\n");
         
         PatoSeñuelo p3 = new PatoSeñuelo();
         p3.mostrar();
         p3.realizarCuaqueo();
+        p3.realizarVuelo();
         p3.nadar();
         System.out.println("\n");
         
         PatoCabezaRoja p4 = new PatoCabezaRoja();
         p4.mostrar();
         p4.realizarCuaqueo();
+        p4.realizarVuelo();
         p4.nadar();
         System.out.println("\n");
     }
